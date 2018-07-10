2a2c7e033cf147573a40e9172af5e2c12738a496
Kyle <kylehawk2@gmail.com>
2018-07-10 14:10:24 -0700

    Second Commit

--- Survey_Form/apps/survey_form/views.py
+++ Survey_Form/apps/survey_form/views.py
@@ -4,7 +4,26 @@ from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
-    
     return render(request, 'survey_form/index.html')
 
-# Create your views here.
+def postinfo(request):
+    if 'count' not in request.session:
+        request.session['count'] = 1
+    else:
+        request.session['count'] += 1
+    print request.session['count']
+    request.session['name'] = request.POST['name']
+    request.session['location'] = request.POST['location']
+    request.session['language'] = request.POST['language']
+    request.session['comment'] = request.POST['comment']
+    return redirect('/result')
+
+def result(request):
+    context = {
+        'count' : request.session['count'],
+        'name' : request.session['name'],
+        'location' : request.session['location'],
+        'comment' : request.session['comment']
+    }
+    print context
+    return render(request, 'result.html', context)
